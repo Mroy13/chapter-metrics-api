83fe315aeca3bbc456a55ae85488d49d9c4bd80d
Mroy13 <mtsprotechguide@gmail.com>
2025-06-06 05:12:08 +0530

    Add cloud set-up for mongodb connection

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
+const {ServerConfig}=require('../config');
 async function Connect(){
    // console.log("inside connect");
-    await mongoose.connect('mongodb://127.0.0.1:27017/ChapterMetricsDB'); 
+    // await mongoose.connect('mongodb://127.0.0.1:27017/ChapterMetricsDB'); 
+
+    await mongoose.connect(`mongodb+srv://mroy13:${ServerConfig.ATLAS_PASSWORD}@cluster0.weydqvd.mongodb.net/ChapterMetricsDB?retryWrites=true&w=majority&appName=Cluster0`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
 }
 module.exports=Connect;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@
  const Connect=require('./config/db-config');
 
  const app=express();
- app.use(express.json({ limit: '10mb' }));
- app.use(express.urlencoded({extended: true},{ limit: '10mb' }));
+ app.use(express.json());
+ app.use(express.urlencoded({extended: true}));
  
 
 
